954d2b981c01d46c890f7081fd613f9bc4633afb
josiegleeson <josiegleeson1@gmail.com>
2024-10-30 10:27:42 +1100

    update icon ui

--- GenomeProt/ui.R
+++ GenomeProt/ui.R
@@ -23,6 +23,10 @@ ui <- dashboardPage(
   dashboardBody(
     useShinyjs(),  # shinyjs
     tags$head(
+      tags$link(rel = "shortcut icon", href = "favicon.ico"),
+      tags$link(rel = "apple-touch-icon", sizes = "180x180", href = "favicon.ico"),
+      tags$link(rel = "icon", type = "image/png", sizes = "32x32", href = "/favicon-32x32.png"),
+      tags$link(rel = "icon", type = "image/png", sizes = "16x16", href = "/favicon-16x16.png"),
       tags$style(HTML("
         .spinner {
           margin: 0 auto;
